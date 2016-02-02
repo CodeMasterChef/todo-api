6db45ccba42e19c90135f6daa86e90dd7be3157f
levanninh@janeto.com <levanninh0101@gmail.com>
2016-02-02 15:36:50 +0700

    fix db

---
+++ playground/variable-playground.js
@@ -0,0 +1,11 @@
+var persion  = {
+    name : "Ninh" ,
+    age : 21
+} ;
+var arrayOfSometing =[ 10 , 20 ] ;
+function  changeString(obj) {
+ obj.push(30) ;
+}
+changeString(arrayOfSometing) ;
+
+console.log(arrayOfSometing) ;
